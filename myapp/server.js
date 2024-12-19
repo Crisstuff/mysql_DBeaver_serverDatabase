9331e494c8882d791bfa6e6af7c62cbc3d7b6de0
Cristoffer Ramsdal <cristofferfolkedalrramsdal@gmail.com>
2024-12-19 09:19:24 +0100

    skrev inn npm install cors og la til mer inn i server.js

--- myapp/server.js
+++ myapp/server.js
@@ -1,3 +1,7 @@
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,3 +13,20 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+const mysql = require('mysql');
+
+// Koble til databasen
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '', // Endre passord hvis nødvendig
+    database: 'express_db'
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Could not connect to database:', err);
+    } else {
+        console.log('Connected to MySQL database.');
+    }
+});
