9b427792dbb08e7fb1199811c7d7666b168d05e8
Cristoffer Ramsdal <cristofferfolkedalrramsdal@gmail.com>
2025-01-08 10:12:51 +0100

    update

---
+++ my-api/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'skole12', // Bytt til ditt MySQL-passord
+    database: 'my_database', // Opprett denne databasen i MySQL
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL!');
+});
+
+
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM users';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+
+app.post('/users', express.json(), (req, res) => {
+    const { name, email } = req.body;
+    const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    db.query(sql, [name, email], (err, result) => {
+        if (err) throw err;
+        res.send('User added successfully');
+    });
+});
+
+
+app.put('/users/:id', express.json(), (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const sql = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+    db.query(sql, [name, email, id], (err, result) => {
+        if (err) throw err;
+        res.send('User updated successfully');
+    });
+});
+
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'DELETE FROM users WHERE id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.send('User deleted successfully');
+    });
+});
+
+
+const cors = require('cors');
+app.use(cors());
+
--- myapp/SQLspørringer/SELECT.js
+++
@@ -1,9 +0,0 @@
-app.get('/users', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json(results);
-    });
- });
- 
